8148594c4f021e40a90e65d58664baad0fa52958
Otyzo <98597929+Otyzo@users.noreply.github.com>
2026-06-25 08:12:42 +0300

    Delete shopmart/login_redirect.php

--- shopmart/login_redirect.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * ShopMart - Login Redirect Helper
- * Week 5: points back to login.php in the same folder, since all
- * ShopMart files (Week 4 + Week 5) live together in one shopmart folder.
- */
-header('Location: login.php');
-exit;
